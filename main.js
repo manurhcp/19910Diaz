362eb41d885384b270fa93972c3883ae7e317fa2
manurhcp <manurhcp04@gmail.com>
2021-09-07 00:39:46 -0300

    Desafío algoritmo simple

---
+++ main.js
@@ -0,0 +1,47 @@
+//Desafío clase: Crear algoritmo JS simple
+
+let nombre = prompt("Ingresa tu nombre");
+let saludo = "¡Hola" + " " + nombre + "!";
+let edadUsuario = prompt("Ingresa tu edad");
+let edad = parseInt(edadUsuario);
+let cinco = 5;
+let sumaEdades = edad + cinco;
+let usuario = prompt("Ingresa tu usuario");
+let contraseña = prompt("Ingresa tu contraseña");
+let usuarioContraseña = "su usuario es" + " " + usuario + " " + "y su contraseña es" + " " + contraseña;
+
+
+console.log(saludo);
+console.log(sumaEdades);
+alert(usuarioContraseña);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
