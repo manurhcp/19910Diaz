55738ba876065ede7e03940cb8a8d6c19ebc80dc
manurhcp <manurhcp04@gmail.com>
2021-09-07 00:52:35 -0300

    se agregan comentarios para faciliar corrección a tutuor

--- main.js
+++ main.js
@@ -1,18 +1,43 @@
-//Desafío clase: Crear algoritmo JS simple
+//DESAFÍO CLASE 1: Crear algoritmo JS simple
 
+//pedir nombre mendiante prompt 
 let nombre = prompt("Ingresa tu nombre");
+
+// texto de saludo
 let saludo = "¡Hola" + " " + nombre + "!";
+
+
+
+
+// número mediante promt
 let edadUsuario = prompt("Ingresa tu edad");
+
+// parseo
 let edad = parseInt(edadUsuario);
+
+// numero en variable
 let cinco = 5;
+
+// suma
 let sumaEdades = edad + cinco;
+
+
+
+
+// pedir textos mediante promt
 let usuario = prompt("Ingresa tu usuario");
 let contraseña = prompt("Ingresa tu contraseña");
+
+// cocatenación
 let usuarioContraseña = "su usuario es" + " " + usuario + " " + "y su contraseña es" + " " + contraseña;
 
-
+// mostrado en consola: nombre y saludo
 console.log(saludo);
+
+// mostrado en consola: edad parseada + número almacenado en variable
 console.log(sumaEdades);
+
+// mostrado en Alerta: concatenación de usuario y contraseña
 alert(usuarioContraseña);
 
 
