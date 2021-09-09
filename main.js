1dd7cbd124819a45c4050b8361874b138d672d32
manurhcp <manurhcp04@gmail.com>
2021-09-08 21:24:50 -0300

    entrega desafio 2

--- main.js
+++ main.js
@@ -1,44 +1,102 @@
-//DESAFÍO CLASE 1: Crear algoritmo JS simple
+// DESAFÍO CLASE 2: Crear un algoritmo con un condicional
 
-//pedir nombre mendiante prompt 
-let nombre = prompt("Ingresa tu nombre");
-
-// texto de saludo
-let saludo = "¡Hola" + " " + nombre + "!";
+let sueldo = prompt ("Indique su salario");
+let sueldoIngresado = parseInt(sueldo);
 
 
-
-
-// número mediante promt
-let edadUsuario = prompt("Ingresa tu edad");
-
-// parseo
-let edad = parseInt(edadUsuario);
-
-// numero en variable
-let cinco = 5;
-
-// suma
-let sumaEdades = edad + cinco;
+if (sueldoIngresado > 1000){
+    alert("Puede comprarse ese par de ojotas que tanto quiere");
+} else if (sueldoIngresado <= 1000){ 
+    alert("Caminará descalzo");
+} else{
+    alert("Ingrese un número");
+}
 
 
 
 
-// pedir textos mediante promt
-let usuario = prompt("Ingresa tu usuario");
-let contraseña = prompt("Ingresa tu contraseña");
+let saludo = prompt("Digame Hola");
 
-// cocatenación
-let usuarioContraseña = "su usuario es" + " " + usuario + " " + "y su contraseña es" + " " + contraseña;
+if (saludo === "Hola"){
+    console.log("Saludo correcto");
 
-// mostrado en consola: nombre y saludo
-console.log(saludo);
+} else {
+    console.log("Lea bien");    
 
-// mostrado en consola: edad parseada + número almacenado en variable
-console.log(sumaEdades);
+}
 
-// mostrado en Alerta: concatenación de usuario y contraseña
-alert(usuarioContraseña);
+
+
+
+let numero = parseInt(prompt("Ingrese número entre 10 y 50"));
+
+   
+if ((numero >= 10) && (numero <= 50)) {
+    alert("Número ingresado está entre 10 y 50");
+    
+} else if ((numero < 10) || (numero > 50)) {
+    alert("Se recomienda repetir primaria") 
+    
+} else {
+    alert("Eso no es un número")
+}
+
+
+
+
+
+
+
+
+// -------------------------------------------------------------------------------------------
+
+// // DESAFÍO CLASE 1: Crear algoritmo JS simple
+
+// //pedir nombre mendiante prompt 
+// let nombre = prompt("Ingresa tu nombre");
+
+// // texto de saludo
+// let saludo = "¡Hola" + " " + nombre + "!";
+
+
+
+
+// // número mediante promt
+// let edadUsuario = prompt("Ingresa tu edad");
+
+// // parseo
+// let edad = parseInt(edadUsuario);
+
+// // numero en variable
+// let cinco = 5;
+
+// // suma
+// let sumaEdades = edad + cinco;
+
+
+
+
+// // pedir textos mediante promt
+// let usuario = prompt("Ingresa tu usuario");
+// let contraseña = prompt("Ingresa tu contraseña");
+
+// // cocatenación
+// let usuarioContraseña = "su usuario es" + " " + usuario + " " + "y su contraseña es" + " " + contraseña;
+
+// // mostrado en consola: nombre y saludo
+// console.log(saludo);
+
+// // mostrado en consola: edad parseada + número almacenado en variable
+// console.log(sumaEdades);
+
+// // mostrado en Alerta: concatenación de usuario y contraseña
+// alert(usuarioContraseña);
+
+// -----------------------------------------------------------------------------------------
+
+
+
+
 
 
 
