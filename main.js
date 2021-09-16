3f42dc2d7b72849a11c76ceea3d9442f8c1f69b0
manurhcp <manurhcp04@gmail.com>
2021-09-16 20:35:59 -0300

    revisión entrega clase 4

--- main.js
+++ main.js
@@ -1,42 +1,156 @@
+// DESAFÍO CLASE 4: Simulador interactivo
 
-// DESAFÍO CLASE 3: Crear un algoritmo utilizando un ciclo
+const opcion1 = "Panes"
+const opcion2 = "Conservas"
 
-/* Pedir número mediante prompt y sumarle otro número en cada repetición,realizando una salida por cada resultado */
+let pan1 = "Pan de campo"
+let pan2 = "Grisines"
+let pan3 = "Pan barra"
+let pan4 = "Pan de molde"
+
+let conserva1 = "Confitado de Tomates"
+let conserva2 = "Escabeche de Porotos"
+let conserva3 = "Escabeche de Pencas"
+let conserva4 = "Escabeche de Zanahorias"
+
+let precio1 = 200
+let precio2 = 100
+let precio3 = 150
+let precio4 = 250
+
+let precio5 = 300
+let precio6 = 150
+let precio7 = 100
+let precio8 = 150
+
+let entradaUsuario = parseInt (prompt("Hola, indíquenos el número de la delicia que desea: \n (1)" + opcion1 + "\n (2)" + opcion2))
+
+let entradaPanes
+
+let entradaConservas
 
 
-alert ("En el campeonato argentino de fútbol se juegan 18 fechas. Vamos a calcular que fechas faltan por jugarse");
+if (entradaUsuario === 1) {
 
-let numeroIngresado = parseInt (prompt ("Ingrese la fecha actual"));
+    entradaPanes = parseInt (prompt("¡Hermosa elección!. Ahora ingrese que panificado desea: \n (1)" + pan1 + "\n (2)" + pan2 + "\n (3)" + pan3 + "\n (4)" + pan4))
 
-for (let i = numeroIngresado; i <= 18; i++) {
-    alert("Falta por jugarse la fecha " + i );
+    }else if (entradaUsuario ===2 ){
+
+        entradaConservas = parseInt (prompt("¡Qué rico!. ¿Qué conservas quiere? \n (1)" + conserva1 + "\n (2)" + conserva2 + "\n (3)" + conserva3 + "\n (4)" + conserva4))
+
+    }else if (entradaUsuario > 2 ){ 
+        alert("El producto no existe")
+    }
+
+
+function ingresoPanes(entradaPanes) {
+
+    switch (entradaPanes) {
+        case 1:
+            return pan1 +" $"+ precio1;
+            break;
+    
+        case 2:
+            return pan2 +" $"+ precio2;
+            break;
+            
+        case 3:
+            return pan3 +" $"+ precio3;
+            break;    
+    
+        case 4:
+            return pan4 +" $"+ precio4;
+            break;    
+        
+        default:
+            return "El producto no existe";
+            break;
+    }
+    
+}
+
+function ingresoConservas(entradaConservas) {
+
+    switch (entradaConservas) {
+        case 1:
+            return conserva1 +" $"+ precio5;
+            break;
+    
+        case 2:
+            return conserva2 +" $"+ precio6;
+            break;
+            
+        case 3:
+            return conserva3 +" $"+ precio7;
+            break;    
+    
+        case 4:
+            return conserva4 +" $"+ precio8;
+            break;    
+        
+        default:
+            return "El producto no existe";
+            break;
+    }
     
 }
 
 
-/* Pedir un texto mediante prompt, concatenar un valor en cada repetición, realizando una salida por cada resultado, hasta que se ingresa “ESC”. */
-
-let candidato = prompt ("Indique su nombre");
-
-let nombreEmpresas = prompt ("Indique la/s empresa/s donde trabajó \n" + "Escriba ESC al finalizar");
-
-while (nombreEmpresas != "ESC") {
-
-   console.log(candidato + " " + nombreEmpresas);
-
-   nombreEmpresas = prompt ("Ingrese la siguiente empresa donde trabajó \n" + "Escriba ESC al finalizar")
-}
-
-/*Pedir un número por prompt, repetir la salida del mensaje “Hola” la cantidad de veces ingresada.*/
-
-let numero = parseInt( prompt("Ingrese un número"))
+console.log(ingresoPanes (entradaPanes));
 
 
-for (let i= 1; i <=  numero  ; i++) {
+console.log(ingresoPanes (entradaConservas));
+        
+    
+
+    
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// // DESAFÍO CLASE 3: Crear un algoritmo utilizando un ciclo
+
+// /* Pedir número mediante prompt y sumarle otro número en cada repetición,realizando una salida por cada resultado */
+
+
+// alert ("En el campeonato argentino de fútbol se juegan 18 fechas. Vamos a calcular que fechas faltan por jugarse");
+
+// let numeroIngresado = parseInt (prompt ("Ingrese la fecha actual"));
+
+// for (let i = numeroIngresado; i <= 18; i++) {
+//     alert("Falta por jugarse la fecha " + i );
+    
+// }
+
+
+// /* Pedir un texto mediante prompt, concatenar un valor en cada repetición, realizando una salida por cada resultado, hasta que se ingresa “ESC”. */
+
+// let candidato = prompt ("Indique su nombre");
+
+// let nombreEmpresas = prompt ("Indique la/s empresa/s donde trabajó \n" + "Escriba ESC al finalizar");
+
+// while (nombreEmpresas != "ESC") {
+
+//    console.log(candidato + " " + nombreEmpresas);
+
+//    nombreEmpresas = prompt ("Ingrese la siguiente empresa donde trabajó \n" + "Escriba ESC al finalizar")
+// }
+
+// /*Pedir un número por prompt, repetir la salida del mensaje “Hola” la cantidad de veces ingresada.*/
+
+// let numero = parseInt( prompt("Ingrese un número"))
+
+
+// for (let i= 1; i <=  numero  ; i++) {
   
-   alert("Hola");
+//    alert("Hola");
    
-}
+// }
 
 
 
